9f13dc9ba6aa6269a6071761ad4a3d41ca109033
Tanushri <matretanushri@gmail.com>
2026-06-17 16:44:03 +0530

    day4

---
+++ oops/Car.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class Car {
+    String color;
+    String brand;
+    String model;
+    int year;
+    int speed;
+    public void speed(){
+        System.out.println("spped is %d per hour",speed);
+    }
+}
---
+++ oops/Test.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Test {
+    public static void main(String[] args) {
+        Car c=new Car();
+        c.color="pink";
+        c.brand="TATA";
+        c.model="12";
+        
+        c.year=1997;
+        c.speed=80;
+        c.speed();
+    }
+}
