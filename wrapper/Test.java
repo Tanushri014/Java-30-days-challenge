406512acd58bc709bc082b83e4603e7ad0bbbb7c
Tanushri <matretanushri@gmail.com>
2026-06-19 18:32:41 +0530

    day8

---
+++ wrapper/Test.java
@@ -0,0 +1,26 @@
+package wrapper;
+
+public class Test {
+    public static void main(String[] args) {
+       int a=3;//primitive type value no methods
+
+
+       //a is stored in stack
+
+//we have to use wrapper classes in collections  because the primitive classes does not support methods 
+
+       //wrapper can perform all the work as primitive 
+       //wrapper is object so it can point to null 
+       //but primitive cant hold null values 
+       //wrapper is object so data is stored in heap 
+       Integer b=8;
+       //autoboxing --directaly convert primitive into object 
+       //but here  integer is a wrapper class which has wrapped a
+       //  primitive type of data into object so that we can provide methods  to it  
+    }
+}
+
+//java is not a purely object oriented language
+
+//wrapper classes 
+//
