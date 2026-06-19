e84855763cbb994f0805703a03e274f9f78d1806
Tanushri <matretanushri@gmail.com>
2026-06-19 18:41:30 +0530

    day9

---
+++ MathOp.java
@@ -0,0 +1,50 @@
+public class MathOp {
+    //java .lang package 
+    public static void main(String[] args) {
+        int a=9;
+        int b=8;
+        int c=-4;
+        //maximum value
+        System.out.println(Math.max(a,b));
+        //minimum value 
+        System.out.println(Math.min(a,b));
+//absolute
+System.out.println(Math.abs(c));
+//greater integer that d
+double d=1.12;
+System.out.println(Math.ceil(d));
+//smallest integer that d
+double e=1.12;
+System.out.println(Math.floor(e));
+//round
+System.out.println(Math.round(1.14));
+
+System.out.println(Math.sqrt(4));
+//sqrt
+//power
+System.out.println(Math.pow(2,3));
+
+//log
+System.out.println(Math.log(10));
+
+//pi
+System.out.println(Math.PI);
+
+//random number
+System.out.println(Math.random()*11);
+
+
+//trigo
+System.out.println(Math.sin(30));
+
+
+
+
+
+
+
+
+
+
+    }
+}
